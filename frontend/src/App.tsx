4019ca55387e79d38433362dbe4bf0d9ddcc361e
Antonio Geraldo Cardoso Junior <>
2022-01-16 12:19:21 -0300

    NavBar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,9 @@
+import {ReactComponent as GithubIcon} from './assets/img/github.svg';
+import NavBar from './componentes/navbar';
 
 function App() {
   return (
-    <h1>Pagina Principal</h1>
+    <NavBar/>
   );
 }
 
